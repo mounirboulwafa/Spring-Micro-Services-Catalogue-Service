a77081e13fc0374251afe4a54df37cf1c87b27df
Mounit BOULWAFA <mounirboulwafa@gmail.com>
2019-10-21 14:42:34 +0100

    Initial commit

---
+++ src/test/java/com/bdcc/catalogueservice/SpringMicroServicesCatalogueServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bdcc.catalogueservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMicroServicesCatalogueServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
